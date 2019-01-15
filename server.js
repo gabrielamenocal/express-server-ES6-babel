80bd71b55c3f4c5c247be5b70adbe8e6ff57a5c5
Gabriela <gmenocal89@gmail.com>
2019-01-14 22:15:41 -0500

    adding post,get,delete,put calls

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 import express from 'express';
-import data from './data/data'
+import data from './data/data';
+
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res) =>
-    res.send('a get call on / route'))
-;
+app.use(express.static('public'));
+app.use('/stuffs', express.static('stuffs'));
+
+
+app.get('/', (req, res) =>
+    // res.send('a get call on / route');
+    res.json(data)
+);
+
+app.put('/', (req, res) =>
+    res.send('a put call on /')
+
+);
+
+app.post('/', (req, res) =>
+    res.send('a post call on /')
+
+);
+
+app.delete('/', (req, res) =>
+    res.send('a delete call on /')
+
+);
+
+
 
 app.listen(PORT, () => {
     console.log('Server running on port' + PORT) ;   
